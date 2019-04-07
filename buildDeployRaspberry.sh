2ff8889a6ab7ae6a70331492271cfd07334acb13
xstahol <staffan.holmgren@tre.se>
2019-04-07 19:58:49 +0200

    deploy scripts

--- buildDeployRaspberry.sh
+++ buildDeployRaspberry.sh
@@ -6,7 +6,7 @@ TARGET_SERVER=raspberryhost
 ./build.sh
 echo '########### Deploying to prod ###########'
 scp target/universal/${APP_NAME}.tgz pi@${TARGET_SERVER}:${WEB_HOME}
-ssh pi@${TARGET_SERVER} "${APP_SCRIPTS_HOME}/bin/STOP_HOMEPAGE.sh;rm -rf ${WEB_HOME}/${APP_NAME};tar -zxf ${WEB_HOME}/${APP_NAME}.tgz -C ${WEB_HOME};${APP_SCRIPTS_HOME}/bin/START_HOMEPAGE.sh;rm ${WEB_HOME}/${APP_NAME}.tgz"
+ssh pi@${TARGET_SERVER} "sudo ${APP_SCRIPTS_HOME}/bin/STOP_HOMEPAGE.sh;rm -rf ${WEB_HOME}/${APP_NAME};tar -zxf ${WEB_HOME}/${APP_NAME}.tgz -C ${WEB_HOME};sudo ${APP_SCRIPTS_HOME}/bin/START_HOMEPAGE.sh;rm ${WEB_HOME}/${APP_NAME}.tgz"
 echo '########### Deploying complete ###########'
 
 
